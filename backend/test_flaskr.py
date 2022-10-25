a4afb9d71656f97548bd5aed2672301ca6055f65
Ghadeer Elsalhawy <54215096+ghadeer-elsalhawy@users.noreply.github.com>
2022-10-25 23:12:47 +0200

    Added Test cases for endpoints and they were successful

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,7 +15,8 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgres://{}/{}".format(
+            'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -24,7 +25,7 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
         """Executed after reach test"""
         pass
@@ -33,6 +34,79 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+
+    # Test for GET '/categories'
+    def test_show_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(len(data['categories']), 6)
+
+    # Done
+
+    # Test for GET '/questions'
+    def test_view_questions(self):
+        res = self.client().get('questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['questions'])
+        self.assertTrue(data['totalQuestions'])
+        self.assertTrue(data['categories'])
+
+    # Done
+
+    # Test for DELETE '/questions/<int:id>'
+    # def test_delete_question(self):
+    #     res = self.client().delete('/questions/2')
+    #     data = json.loads(res.data)
+
+    #     self.assertEqual(data['success'], True)
+
+    # Done
+
+    # Test for DELETE '/questions/<int:id>' with invalid question id
+    def test_delete_question_invalid_id(self):
+        res = self.client().delete('/questions/999')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], "Unprocessable recource")
+
+    # Done
+
+    # Test for POST '/questions'
+    def test_add_question(self):
+        res = self.client().post('/questions', json={
+            'question':  'Heres a new question string',
+            'answer':  'Heres a new answer string',
+            'difficulty': 1,
+            'category': 3,
+        })
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['totalQuestions'])
+        self.assertTrue(data['questions'])
+
+    # Done
+
+    # Test for POST 'search'
+    # def test_search_question(self):
+    #     res = self.client().post('/search', json={
+    #         'searchTerm': 'is'
+    #     })
+    #     data = json.loads(res.data)
+
+    #     self.assertEqual(res.status_code, 200)
+    #     self.assertEqual(data['success'], True)
+    #     self.assertTrue(data['questions'])
+    #     self.assertTrue(data['totalQuestions'])
 
 
 # Make the tests conveniently executable
